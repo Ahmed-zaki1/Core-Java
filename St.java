5a90a82b63c6d5683ae6f6db7551b76ad4c3777a
Ahmed zaki <zaki40886@gmail.com>
2020-11-26 09:56:47 +0530

    Ahmed

---
+++ St.java
@@ -0,0 +1,8 @@
+class St{
+public static void main(String[]args){
+String s="Hello";
+//String s=new String("Bye");
+
+System.out.println(s);
+}
+}
---
+++ SwapInString.java
@@ -0,0 +1,13 @@
+public class SwapInString     
+{    
+    public static void main(String[] args) {    
+        String str1 = "Good ", str2 = "morning ";    
+         System.out.println("before swapping: " + str1 + " " + str2);    
+        
+        str1 = str1 + str2;    
+        str2 = str1.substring(0, (str1.length() - str2.length()));    
+        str1 = str1.substring(str2.length());    
+            
+        System.out.println("after swapping: " + str1 + " " + str2);    
+    }    
+}    
